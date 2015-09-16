4b97964da731d2f93ef9e7b42828d8941636e29a
noreply <noreply@example.com>
2015-09-16 03:22:53 -0400

    add app id

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd Telegram
 
+# setup api key
+sed -i "s/int APP_ID = 0;/int APP_ID = $APP_ID;/g" TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+sed -i "s/APP_HASH = \"\";/APP_HASH = \"$APP_HASH\";/g" TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+
 if [ ! -f "TMessagesProj/config/debug.keystore" ]; then
     keytool -genkey -v -keystore TMessagesProj/config/debug.keystore -storepass android -alias androiddebugkey -keypass android -dname "CN=Android Debug,O=Android,C=US"
 fi
