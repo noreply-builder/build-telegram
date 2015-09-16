79f541b209bbf9a3f2e5a41cffc7d12deca99f52
noreply-builder <noreply.watcher@gmail.com>
2015-09-16 12:02:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd Telegram
 
+if [ ! -f "TMessagesProj/config/debug.keystore" ]; then
+    keytool -genkey -v -keystore TMessagesProj/config/debug.keystore -storepass android -alias androiddebugkey -keypass android -dname "CN=Android Debug,O=Android,C=US"
+fi
+
 #gradle clean build
 gradle clean assembleDebug
 
