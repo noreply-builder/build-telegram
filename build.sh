b356f895dcd1b3781952e6004fef05d7d639bfa1
noreply-builder <noreply.watcher@gmail.com>
2015-09-16 12:50:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ fi
 #gradle clean build
 gradle clean assembleDebug
 
-ls -lR app/build/outputs
+ls -lR TMessagesProj/build/outputs
 
-cp app/build/outputs/apk/app-debug.apk $HOME/Telegram-v3.1.3.apk
+cp TMessagesProj/build/outputs/apk/app-debug.apk $HOME/Telegram-v3.1.3.apk
